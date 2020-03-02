5bb25e0552ea70fec858d94ec0e840337da00ebe
kubbit98 <Mordek1998>
2020-03-02 11:37:23 +0100

    Init commit

---
+++ Cw1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+
+namespace Cw1
+{
+    public class Program
+    {
+        static async System.Threading.Tasks.Task Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            //int? tmp = null;
+            //double tmp2 = 2;
+            //string tmp3 = "Aaaa";
+            //bool tmp4 = true;
+            //var tmp5 = "maslo";
+            //var newPerson = new Person { FirstName = "Janek" };
+
+            var url = args.Length>0?args[0]:"http://pja.edu.pl";
+            var httpClient = new HttpClient();
+            var response = await httpClient.GetAsync(url);
+            if (response.IsSuccessStatusCode)
+            {
+                var htmlContent = await response.Content.ReadAsStringAsync();
+                var regex = new Regex("[a-z]+[a-z0-9]*@[a-z0-9]+\\.[a-z]+", RegexOptions.IgnoreCase);
+                var matches = regex.Matches(htmlContent);
+                foreach (var match in matches)
+                {
+                    Console.WriteLine(match.ToString());
+                }
+            }
+        }
+    }
+}
